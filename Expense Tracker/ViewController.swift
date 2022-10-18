0d33fa5251961f1638bbbf4f104afc99cb14e2a4
CaseyBot <bernalcasey@gmail.com>
2022-10-17 20:38:58 -0500

    core data and file

--- Expense Tracker/ViewController.swift
+++ Expense Tracker/ViewController.swift
@@ -7,11 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        //cell.delegate = self
+        
+        return cell
+    }
+    
+    @IBOutlet weak var homeView: UITableView!
+    
     override func viewDidLoad() {
         ///
         super.viewDidLoad()
+        
+        homeView.delegate = self
+        homeView.dataSource = self
+        
         // Do any additional setup after loading the view.
     }
 
